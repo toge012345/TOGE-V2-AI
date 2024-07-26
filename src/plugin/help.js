fa7189d870f0bb4a9b7cb430ff192b3edbaff6b9
𝕋𝕆𝔾𝔼 𝕀ℕ𝕌𝕄𝔸𝕂𝕀 <167923490+toge012345@users.noreply.github.com>
2024-07-26 18:58:56 +0100

    𝗧𝗢𝗚𝗘-𝗠𝗗-𝗩𝟮

--- src/plugin/help.js
+++ src/plugin/help.js
@@ -92,11 +92,6 @@ let mode = ethix.public ? 'public' : 'private';
             body: proto.Message.InteractiveMessage.Body.create({
               text: ` 
 ⍟────────────────⍟
-*REMEMBER THIS BOT IS FOR*
-*EDUCATIONAL PERPOSES*
-⍟────────────────⍟
-*© TOGE INUMAKI*
-⍟────────────────⍟
 *_Konnichiwa! Its TOGE-MD-V2_* 
 *_Multi-Device Whatsapp BOT_*
 *_Recoded By TOGE INUMAKI_*
@@ -108,9 +103,6 @@ let mode = ethix.public ? 'public' : 'private';
 ╋┗┛┗━┻━━┻━┛╋┗┻━┻┻━━┛
 
 ╭─────────────────⦿
-┃    🔰〘 𝙄𝙉𝙁𝙊 〙🔰
-╰┬────────────────⦿
-┌┤
 ┃   *Founder:* ᴛᴏɢᴇ ɪɴᴜᴍᴀᴋɪ
 ┃   *Bot Name:* 𝗧𝗢𝗚𝗘-𝗠𝗗-𝗩𝟮 
 ┃   *Host:* Kali Linux
@@ -118,13 +110,8 @@ let mode = ethix.public ? 'public' : 'private';
 ┃   *Uptime:* ${uptime} 
 ╰───────────────⍟
 
-╭──────────────────⦿
-┃   🔰〘 𝑰𝑵𝑭𝑶 𝑪𝑴𝑫 〙🔰
-╰┬─────────────────⦿
-┌┤ 
-┃ *SELECT MENU*
-│
-╰──────────────────⦿`
+⌕ ❙❘❙❙❘❙❚❙❘❙❙❚❙❘❙❘❙❚❙❘❙❙❚❙❘❙❙❘❙❚❙❘ ⌕
+`
             }),
             footer: proto.Message.InteractiveMessage.Footer.create({
               text: "ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴛᴏɢᴇ-ᴍᴅ-ᴠ2"
@@ -234,11 +221,6 @@ let mode = ethix.public ? 'public' : 'private';
         const mode = process.env.MODE;
         const str = `hey ${m.pushName} ${pushwish}
 ⍟────────────────⍟
-*REMEMBER THIS BOT IS FOR*
-*EDUCATIONAL PERPOSES*
-⍟────────────────⍟
-*© TOGE INUMAKI*
-⍟────────────────⍟
 *_Konnichiwa! Its TOGE-MD-V2_* 
 *_Multi-Device Whatsapp BOT_*
 *_Recoded By TOGE INUMAKI_*
@@ -250,9 +232,6 @@ let mode = ethix.public ? 'public' : 'private';
 ╋┗┛┗━┻━━┻━┛╋┗┻━┻┻━━┛
 
 ╭─────────────────⦿
-┃    🔰〘 𝙄𝙉𝙁𝙊 〙🔰
-╰┬────────────────⦿
-┌┤
 ┃   *Founder:* ᴛᴏɢᴇ ɪɴᴜᴍᴀᴋɪ
 ┃   *Bot Name:* 𝗧𝗢𝗚𝗘-𝗠𝗗-𝗩𝟮 
 ┃   *Host:* Kali Linux
@@ -260,138 +239,106 @@ let mode = ethix.public ? 'public' : 'private';
 ┃   *Uptime:* ${uptime} 
 ╰───────────────⍟
 
-╭──────────────────⦿
-┃   🔰〘 𝑰𝑵𝑭𝑶 𝑪𝑴𝑫 〙🔰
-╰┬─────────────────⦿
-┌┤ 
-┃ *ALL MENU*
-│
-╰──────────────────⦿
-╭─────────────────⦿
-┃ 🔰〘 𝗖𝗢𝗡𝗩𝗘𝗥𝗧𝗘𝗥 〙🔰
-╰┬────────────────⦿
-┌┤
-┃✰ ${prefix}𝙰𝚃𝚃𝙿
-┃✰ ${prefix}𝙰𝚃𝚃𝙿2
-┃✰ ${prefix}𝙰𝚃𝚃𝙿3
-┃✰ ${prefix}𝙴𝙱𝙸𝙽𝙰𝚁𝚈
-┃✰ ${prefix}𝙳𝙱𝙸𝙽𝙰𝚁𝚈
-┃✰ ${prefix}𝙴𝙼𝙾𝙹𝙸𝙼𝙸𝚇
-┃✰ ${prefix}𝙼𝙿3
+⌕ ❙❘❙❙❘❙❚❙❘❙❙❚❙❘❙❘❙❚❙❘❙❙❚❙❘❙❙❘❙❚❙❘ ⌕
+
+┠┌─⦿ 𝙲𝙾𝙽𝚅𝙴𝚁𝚃𝙴𝚁 ⦿
+┃│❏ ${prefix}𝙰𝚃𝚃𝙿
+┃│❏ ${prefix}𝙰𝚃𝚃𝙿2
+┃│❏ ${prefix}𝙰𝚃𝚃𝙿3
+┃│❏ ${prefix}𝙴𝙱𝙸𝙽𝙰𝚁𝚈
+┃│❏ ${prefix}𝙳𝙱𝙸𝙽𝙰𝚁𝚈
+┃│❏ ${prefix}𝙴𝙼𝙾𝙹𝙸𝙼𝙸𝚇
+┃│❏ ${prefix}𝙼𝙿3
 ╰───────────────⍟
-╭─────────────────⦿
-┃ 🔰〘 𝗔𝗜 〙🔰
-╰┬────────────────⦿
-┌┤
-┃✰ ${prefix}𝙰𝚒
-┃✰ ${prefix}𝙱𝚞𝚐
-┃✰ ${prefix}𝚁𝚎𝚙𝚘𝚛𝚝
-┃✰ ${prefix}𝙶𝚙𝚝
-┃✰ ${prefix}𝙳𝚊𝚕𝚕𝚎
-┃✰ ${prefix}𝚁𝚎𝚖𝚒𝚗𝚒
-┃✰ ${prefix}𝙶𝚎𝚖𝚒𝚗𝚒
+┠┌─⦿ 𝙰𝙸 ⦿
+┃│❏ ${prefix}𝙰𝚒
+┃│❏ ${prefix}𝙱𝚞𝚐
+┃│❏ ${prefix}𝚁𝚎𝚙𝚘𝚛𝚝
+┃│❏ ${prefix}𝙶𝚙𝚝
+┃│❏ ${prefix}𝙳𝚊𝚕𝚕𝚎
+┃│❏ ${prefix}𝚁𝚎𝚖𝚒𝚗𝚒
+┃│❏ ${prefix}𝙶𝚎𝚖𝚒𝚗𝚒
 ╰───────────────⍟
-╭─────────────────⦿
-┃ 🔰〘 𝗧𝗢𝗢𝗟𝗦 〙🔰
-╰┬────────────────⦿
-┌┤
-┃✰ ${prefix}𝙲𝚊𝚕𝚌𝚞𝚕𝚊𝚝𝚘𝚛
-┃✰ ${prefix}𝚃𝚎𝚖𝚙𝚖𝚊𝚒𝚕
-┃✰ ${prefix}𝙲𝚑𝚎𝚌𝚔𝚖𝚊𝚒𝚕
-┃✰ ${prefix}𝚃𝚛𝚝
-┃✰ ${prefix}𝚃𝚝𝚜
+┠┌─⦿ 𝚃𝙾𝙾𝙻 ⦿
+┃│❏ ${prefix}𝙲𝚊𝚕𝚌𝚞𝚕𝚊𝚝𝚘𝚛
+┃│❏ ${prefix}𝚃𝚎𝚖𝚙𝚖𝚊𝚒𝚕
+┃│❏ ${prefix}𝙲𝚑𝚎𝚌𝚔𝚖𝚊𝚒𝚕
+┃│❏ ${prefix}𝚃𝚛𝚝
+┃│❏ ${prefix}𝚃𝚝𝚜
 ╰───────────────⍟
-╭─────────────────⦿
-┃ 🔰〘 𝗚𝗥𝗢𝗨𝗣 〙🔰
-╰┬────────────────⦿
-┌┤
-┃✰ ${prefix}𝙻𝚒𝚗𝚔𝙶𝚛𝚘𝚞𝚙
-┃✰ ${prefix}𝚂𝚎𝚝𝚙𝚙𝚐𝚌
-┃✰ ${prefix}𝚂𝚎𝚝𝚗𝚊𝚖𝚎
-┃✰ ${prefix}𝚂𝚎𝚝𝚍𝚎𝚜𝚌
-┃✰ ${prefix}𝙶𝚛𝚘𝚞𝚙
-┃✰ ${prefix}𝙶𝚌𝚜𝚎𝚝𝚝𝚒𝚗𝚐
-┃✰ ${prefix}𝚆𝚎𝚕𝚌𝚘𝚖𝚎
-┃✰ ${prefix}𝙰𝚍𝚍
-┃✰ ${prefix}𝙺𝚒𝚌𝚔
-┃✰ ${prefix}𝙷𝚒𝚍𝚎𝚃𝚊𝚐
-┃✰ ${prefix}𝚃𝚊𝚐𝚊𝚕𝚕
-┃✰ ${prefix}𝙰𝚗𝚝𝚒𝙻𝚒𝚗𝚔
-┃✰ ${prefix}𝙰𝚗𝚝𝚒𝚃𝚘𝚡𝚒𝚌
-┃✰ ${prefix}𝙿𝚛𝚘𝚖𝚘𝚝𝚎
-┃✰ ${prefix}𝙳𝚎𝚖𝚘𝚝𝚎
-┃✰ ${prefix}𝙶𝚎𝚝𝚋𝚒𝚘
+┠┌─⦿ 𝙶𝚁𝙾𝚄𝙿 ⦿
+┃│❏ ${prefix}𝙻𝚒𝚗𝚔𝙶𝚛𝚘𝚞𝚙
+┃│❏ ${prefix}𝚂𝚎𝚝𝚙𝚙𝚐𝚌
+┃│❏ ${prefix}𝚂𝚎𝚝𝚗𝚊𝚖𝚎
+┃│❏ ${prefix}𝚂𝚎𝚝𝚍𝚎𝚜𝚌
+┃│❏ ${prefix}𝙶𝚛𝚘𝚞𝚙
+┃│❏ ${prefix}𝙶𝚌𝚜𝚎𝚝𝚝𝚒𝚗𝚐
+┃│❏ ${prefix}𝚆𝚎𝚕𝚌𝚘𝚖𝚎
+┃│❏ ${prefix}𝙰𝚍𝚍
+┃│❏ ${prefix}𝙺𝚒𝚌𝚔
+┃│❏ ${prefix}𝙷𝚒𝚍𝚎𝚃𝚊𝚐
+┃│❏ ${prefix}𝚃𝚊𝚐𝚊𝚕𝚕
+┃│❏ ${prefix}𝙰𝚗𝚝𝚒𝙻𝚒𝚗𝚔
+┃│❏ ${prefix}𝙰𝚗𝚝𝚒𝚃𝚘𝚡𝚒𝚌
+┃│❏ ${prefix}𝙿𝚛𝚘𝚖𝚘𝚝𝚎
+┃│❏ ${prefix}𝙳𝚎𝚖𝚘𝚝𝚎
+┃│❏ ${prefix}𝙶𝚎𝚝𝚋𝚒𝚘
 ╰───────────────⍟
-╭─────────────────⦿
-┃ 🔰〘 𝗗𝗢𝗪𝗡𝗟𝗢𝗔𝗗 〙🔰
-╰┬────────────────⦿
-┌┤
-┃✰ ${prefix}𝙰𝚙𝚔
-┃✰ ${prefix}𝙵𝚊𝚌𝚎𝚋𝚘𝚘𝚔
-┃✰ ${prefix}𝙼𝚎𝚍𝚒𝚊𝚏𝚒𝚛𝚎
-┃✰ ${prefix}𝙿𝚒𝚗𝚝𝚎𝚛𝚎𝚜𝚝𝚍𝚕
-┃✰ ${prefix}𝙶𝚒𝚝𝚌𝚕𝚘𝚗𝚎
-┃✰ ${prefix}𝙶𝚍𝚛𝚒𝚟𝚎
-┃✰ ${prefix}𝙸𝚗𝚜𝚝𝚊
-┃✰ ${prefix}𝚈𝚝𝚖𝚙3
-┃✰ ${prefix}𝚈𝚝𝚖𝚙4
-┃✰ ${prefix}𝙿𝚕𝚊𝚢
-┃✰ ${prefix}𝚂𝚘𝚗𝚐
-┃✰ ${prefix}𝚅𝚒𝚍𝚎𝚘
-┃✰ ${prefix}𝚈𝚝𝚖𝚙3𝚍𝚘𝚌
-┃✰ ${prefix}𝚈𝚝𝚖𝚙4𝚍𝚘𝚌
-┃✰ ${prefix}𝚃𝚒𝚔𝚝𝚘𝚔
+┠┌─⦿ 𝙳𝙾𝚆𝙽𝙻𝙾𝙰𝙳 ⦿
+┃│❏ ${prefix}𝙰𝚙𝚔
+┃│❏ ${prefix}𝙵𝚊𝚌𝚎𝚋𝚘𝚘𝚔
+┃│❏ ${prefix}𝙼𝚎𝚍𝚒𝚊𝚏𝚒𝚛𝚎
+┃│❏ ${prefix}𝙿𝚒𝚗𝚝𝚎𝚛𝚎𝚜𝚝𝚍𝚕
+┃│❏ ${prefix}𝙶𝚒𝚝𝚌𝚕𝚘𝚗𝚎
+┃│❏ ${prefix}𝙶𝚍𝚛𝚒𝚟𝚎
+┃│❏ ${prefix}𝙸𝚗𝚜𝚝𝚊
+┃│❏ ${prefix}𝚈𝚝𝚖𝚙3
+┃│❏ ${prefix}𝚈𝚝𝚖𝚙4
+┃│❏ ${prefix}𝙿𝚕𝚊𝚢
+┃│❏ ${prefix}𝚂𝚘𝚗𝚐
+┃│❏ ${prefix}𝚅𝚒𝚍𝚎𝚘
+┃│❏ ${prefix}𝚈𝚝𝚖𝚙3𝚍𝚘𝚌
+┃│❏ ${prefix}𝚈𝚝𝚖𝚙4𝚍𝚘𝚌
+┃│❏ ${prefix}𝚃𝚒𝚔𝚝𝚘𝚔
 ╰───────────────⍟
-╭─────────────────⦿
-┃ 🔰〘 𝗦𝗘𝗔𝗥𝗖𝗛 〙🔰
-╰┬────────────────⦿
-┌┤
-┃✰ ${prefix}𝙿𝚕𝚊𝚢
-┃✰ ${prefix}𝚈𝚝𝚜
-┃✰ ${prefix}𝙸𝚖𝚍𝚋
-┃✰ ${prefix}𝙶𝚘𝚘𝚐𝚕𝚎
-┃✰ ${prefix}𝙶𝚒𝚖𝚊𝚐𝚎
-┃✰ ${prefix}𝙿𝚒𝚗𝚝𝚎𝚛𝚎𝚜𝚝
-┃✰ ${prefix}𝚆𝚊𝚕𝚕𝚙𝚊𝚙𝚎𝚛
-┃✰ ${prefix}𝚆𝚒𝚔𝚒𝚖𝚎𝚍𝚒𝚊
-┃✰ ${prefix}𝚈𝚝𝚜𝚎𝚊𝚛𝚌𝚑
-┃✰ ${prefix}𝚁𝚒𝚗𝚐𝚝𝚘𝚗𝚎
-┃✰ ${prefix}𝙻𝚢𝚛𝚒𝚌𝚜
+┠┌─⦿ 𝚂𝙴𝙰𝚁𝙲𝙷 ⦿
+┃│❏ ${prefix}𝙿𝚕𝚊𝚢
+┃│❏ ${prefix}𝚈𝚝𝚜
+┃│❏ ${prefix}𝙸𝚖𝚍𝚋
+┃│❏ ${prefix}𝙶𝚘𝚘𝚐𝚕𝚎
+┃│❏ ${prefix}𝙶𝚒𝚖𝚊𝚐𝚎
+┃│❏ ${prefix}𝙿𝚒𝚗𝚝𝚎𝚛𝚎𝚜𝚝
+┃│❏ ${prefix}𝚆𝚊𝚕𝚕𝚙𝚊𝚙𝚎𝚛
+┃│❏ ${prefix}𝚆𝚒𝚔𝚒𝚖𝚎𝚍𝚒𝚊
+┃│❏ ${prefix}𝚈𝚝𝚜𝚎𝚊𝚛𝚌𝚑
+┃│❏ ${prefix}𝚁𝚒𝚗𝚐𝚝𝚘𝚗𝚎
+┃│❏ ${prefix}𝙻𝚢𝚛𝚒𝚌𝚜
 ╰───────────────⍟
-╭─────────────────⦿
-┃ 🔰〘 𝗠𝗔𝗜𝗡 〙🔰
-╰┬────────────────⦿
-┌┤
-┃✰ ${prefix}𝙿𝚒𝚗𝚐
-┃✰ ${prefix}𝙰𝚕𝚒𝚟𝚎
-┃✰ ${prefix}𝙾𝚠𝚗𝚎𝚛
-┃✰ ${prefix}𝙼𝚎𝚗𝚞
-┃✰ ${prefix}𝙸𝚗𝚏𝚘𝚋𝚘𝚝
+┠┌─⦿ 𝙼𝙰𝙸𝙽 ⦿
+┃│❏ ${prefix}𝙿𝚒𝚗𝚐
+┃│❏ ${prefix}𝙰𝚕𝚒𝚟𝚎
+┃│❏ ${prefix}𝙾𝚠𝚗𝚎𝚛
+┃│❏ ${prefix}𝙼𝚎𝚗𝚞
+┃│❏ ${prefix}𝙸𝚗𝚏𝚘𝚋𝚘𝚝
 ╰───────────────⍟
-╭─────────────────⦿
-┃ 🔰〘 𝗢𝗪𝗡𝗘𝗥 〙🔰
-╰┬────────────────⦿
-┌┤
-┃✰ ${prefix}𝙹𝚘𝚒𝚗
-┃✰ ${prefix}𝙻𝚎𝚊𝚟𝚎
-┃✰ ${prefix}𝙱𝚕𝚘𝚌𝚔
-┃✰ ${prefix}𝚄𝚗𝚋𝚕𝚘𝚌𝚔
-┃✰ ${prefix}𝚂𝚎𝚝𝚙𝚙𝚋𝚘𝚝
-┃✰ ${prefix}𝙰𝚗𝚝𝚒𝚌𝚊𝚕𝚕
-┃✰ ${prefix}𝚂𝚎𝚝𝚜𝚝𝚊𝚝𝚞𝚜
-┃✰ ${prefix}𝚂𝚎𝚝𝚗𝚊𝚖𝚎𝚋𝚘𝚝
-┃✰ ${prefix}𝙰𝚞𝚝𝚘𝚃𝚢𝚙𝚒𝚗𝚐
-┃✰ ${prefix}𝙰𝚕𝚠𝚊𝚢𝚜𝙾𝚗𝚕𝚒𝚗𝚎
-┃✰ ${prefix}𝙰𝚞𝚝𝚘𝚁𝚎𝚊𝚍
-┃✰ ${prefix}𝚊𝚞𝚝𝚘𝚜𝚟𝚒𝚎𝚠
+┠┌─⦿ 𝙾𝚆𝙽𝙴𝚁 ⦿
+┃│❏ ${prefix}𝙹𝚘𝚒𝚗
+┃│❏ ${prefix}𝙻𝚎𝚊𝚟𝚎
+┃│❏ ${prefix}𝙱𝚕𝚘𝚌𝚔
+┃│❏ ${prefix}𝚄𝚗𝚋𝚕𝚘𝚌𝚔
+┃│❏ ${prefix}𝚂𝚎𝚝𝚙𝚙𝚋𝚘𝚝
+┃│❏ ${prefix}𝙰𝚗𝚝𝚒𝚌𝚊𝚕𝚕
+┃│❏ ${prefix}𝚂𝚎𝚝𝚜𝚝𝚊𝚝𝚞𝚜
+┃│❏ ${prefix}𝚂𝚎𝚝𝚗𝚊𝚖𝚎𝚋𝚘𝚝
+┃│❏ ${prefix}𝙰𝚞𝚝𝚘𝚃𝚢𝚙𝚒𝚗𝚐
+┃│❏ ${prefix}𝙰𝚕𝚠𝚊𝚢𝚜𝙾𝚗𝚕𝚒𝚗𝚎
+┃│❏ ${prefix}𝙰𝚞𝚝𝚘𝚁𝚎𝚊𝚍
+┃│❏ ${prefix}𝚊𝚞𝚝𝚘𝚜𝚟𝚒𝚎𝚠
 ╰───────────────⍟
-╭─────────────────⦿
-┃ 🔰〘 𝗦𝗧𝗔𝗟𝗞 〙🔰
-╰┬────────────────⦿
-┌┤
-┃✰ ${prefix}𝚃𝚛𝚞𝚎𝚌𝚊𝚕𝚕𝚎𝚛
-┃✰ ${prefix}𝙸𝚗𝚜𝚝𝚊𝚂𝚝𝚊𝚕𝚔
-┃✰ ${prefix}𝙶𝚒𝚝𝚑𝚞𝚋𝚂𝚝𝚊𝚕𝚔
+┠┌─⦿ 𝚂𝚃𝙰𝙻𝙺 ⦿
+┃│ ${prefix}𝚃𝚛𝚞𝚎𝚌𝚊𝚕𝚕𝚎𝚛
+┃│ ${prefix}𝙸𝚗𝚜𝚝𝚊𝚂𝚝𝚊𝚕𝚔
+┃│ ${prefix}𝙶𝚒𝚝𝚑𝚞𝚋𝚂𝚝𝚊𝚕𝚔
 ╰───────────────⍟
    `;
         let fgg = {
