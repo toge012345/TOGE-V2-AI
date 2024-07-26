fd8ca23be2f5a063fcf4a7d0155228236b7ec246
𝕋𝕆𝔾𝔼 𝕀ℕ𝕌𝕄𝔸𝕂𝕀 <167923490+toge012345@users.noreply.github.com>
2024-07-26 13:18:51 +0100

    𝗧𝗢𝗚𝗘-𝗠𝗗-𝗩𝟮

--- src/plugin/block.js
+++ src/plugin/block.js
@@ -13,7 +13,7 @@ const block = async (m, gss) => {
 
     if (!validCommands.includes(cmd)) return;
     
-    if (!isCreator) return m.reply("*📛 THIS IS AN OWNER COMMAND*");
+    if (!isCreator) return m.reply("*🔰 THIS IS AN OWNER COMMAND 🔰*");
 
     let users = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : text.replace(/[^0-9]/g, '') + '@s.whatsapp.net';
     
