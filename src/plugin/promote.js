e767c200534ca082a7fcf2e58837c9b47ce7aec5
𝕋𝕆𝔾𝔼 𝕀ℕ𝕌𝕄𝔸𝕂𝕀 <167923490+toge012345@users.noreply.github.com>
2024-07-26 20:51:06 +0100

    𝗧𝗢𝗚𝗘-𝗠𝗗-𝗩𝟮

--- src/plugin/promote.js
+++ src/plugin/promote.js
@@ -10,14 +10,14 @@ const promote = async (m, gss) => {
 
     if (!validCommands.includes(cmd)) return;
 
-    if (!m.isGroup) return m.reply("*🚫 THIS COMMAND CAN ONLY BE USED IN GROUPS*");
+    if (!m.isGroup) return m.reply("*🔰 THIS COMMAND CAN ONLY BE USED IN GROUPS 🔰*");
     const groupMetadata = await gss.groupMetadata(m.from);
     const participants = groupMetadata.participants;
     const botAdmin = participants.find(p => p.id === botNumber)?.admin;
     const senderAdmin = participants.find(p => p.id === m.sender)?.admin;
 
-    if (!botAdmin) return m.reply("*🚫 BOT MUST BE AN ADMIN TO USE THIS COMMAND*");
-    if (!senderAdmin) return m.reply("*🚫 YOU MUST BE AN ADMIN TO USE THIS COMMAND*");
+    if (!botAdmin) return m.reply("*🔰 BOT MUST BE AN ADMIN TO USE THIS COMMAND 🔰*");
+    if (!senderAdmin) return m.reply("*🔰 YOU MUST BE AN ADMIN TO USE THIS COMMAND 🔰*");
 
     if (!m.mentionedJid) m.mentionedJid = [];
 
@@ -30,7 +30,7 @@ const promote = async (m, gss) => {
       : [];
 
     if (users.length === 0) {
-      return m.reply("*🚫 PLEASE MENTION OR QUOTE A USER TO PROMOTE*");
+      return m.reply("*🔰 PLEASE MENTION OR QUOTE A USER TO PROMOTE 🔰*");
     }
     console.log('users: ', users)
     const validUsers = users.filter(Boolean);
