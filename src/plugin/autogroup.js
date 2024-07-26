148ff65ad31f0167306a97401ca066c74e38ac54
𝕋𝕆𝔾𝔼 𝕀ℕ𝕌𝕄𝔸𝕂𝕀 <167923490+toge012345@users.noreply.github.com>
2024-07-26 21:17:06 +0100

    𝗧𝗢𝗚𝗘-𝗠𝗗-𝗩𝟮

--- src/plugin/autogroup.js
+++ src/plugin/autogroup.js
@@ -12,15 +12,15 @@ const groupSetting = async (m, gss) => {
     const validCommands = ['group'];
     if (!validCommands.includes(cmd)) return;
 
-    if (!m.isGroup) return m.reply("*📛 THIS COMMAND CAN ONLY BE USED IN GROUPS*");
+    if (!m.isGroup) return m.reply("*🔰 THIS COMMAND CAN ONLY BE USED IN GROUPS 🔰*");
     const groupMetadata = await gss.groupMetadata(m.from);
     const participants = groupMetadata.participants;
     const botNumber = await gss.decodeJid(gss.user.id);
     const botAdmin = participants.find(p => p.id === botNumber)?.admin;
     const senderAdmin = participants.find(p => p.id === m.sender)?.admin;
 
-    if (!botAdmin) return m.reply("*📛 BOT MUST BE AN ADMIN TO USE THIS COMMAND*");
-    if (!senderAdmin) return m.reply("*📛 YOU MUST BE AN ADMIN TO USE THIS COMMAND*");
+    if (!botAdmin) return m.reply("*🔰 BOT MUST BE AN ADMIN TO USE THIS COMMAND 🔰*");
+    if (!senderAdmin) return m.reply("*🔰 YOU MUST BE AN ADMIN TO USE THIS COMMAND 🔰*");
 
     const args = m.body.slice(prefix.length + cmd.length).trim().split(/\s+/);
     if (args.length < 1) return m.reply(`Please specify a setting (open/close) and optionally a time.\n\nExample:\n*${prefix + cmd} open* or *${prefix + cmd} open 04:00 PM*`);
