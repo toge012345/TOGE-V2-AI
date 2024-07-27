cf0c71a8c81eaacda42f64c215b9eb14925c46e1
𝕋𝕆𝔾𝔼 𝕀ℕ𝕌𝕄𝔸𝕂𝕀 <167923490+toge012345@users.noreply.github.com>
2024-07-27 10:33:07 +0100

    𝗧𝗢𝗚𝗘-𝗠𝗗-𝗩𝟮

--- src/plugin/gcfullpp.js
+++ src/plugin/gcfullpp.js
@@ -12,15 +12,15 @@ const setProfilePictureGroup = async (m, gss) => {
 
   if (validCommands.includes(cmd)) {
     
-    if (!m.isGroup) return m.reply("*📛 THIS COMMAND CAN ONLY BE USED IN GROUPS*");
+    if (!m.isGroup) return m.reply("*🔰 THIS COMMAND CAN ONLY BE USED IN GROUPS 🔰*");
     const groupMetadata = await gss.groupMetadata(m.from);
     const participants = groupMetadata.participants;
     const botNumber = await gss.decodeJid(gss.user.id);
     const botAdmin = participants.find(p => p.id === botNumber)?.admin;
     const senderAdmin = participants.find(p => p.id === m.sender)?.admin;
 
-    if (!botAdmin) return m.reply("*📛 BOT MUST BE AN ADMIN TO USE THIS COMMAND*");
-    if (!senderAdmin) return m.reply("*📛 YOU MUST BE AN ADMIN TO USE THIS COMMAND*");
+    if (!botAdmin) return m.reply("*🔰 BOT MUST BE AN ADMIN TO USE THIS COMMAND 🔰*");
+    if (!senderAdmin) return m.reply("*🔰 YOU MUST BE AN ADMIN TO USE THIS COMMAND 🔰*");
     if (!m.quoted || m.quoted.mtype !== 'imageMessage') {
       return m.reply(`Send/Reply with an image to set your profile picture ${prefix + cmd}`);
     }
