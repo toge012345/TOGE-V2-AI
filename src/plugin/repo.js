017b77ccc5d681f9c2b66a809ed3057ebc299d17
𝕋𝕆𝔾𝔼 𝕀ℕ𝕌𝕄𝔸𝕂𝕀 <167923490+toge012345@users.noreply.github.com>
2024-08-13 16:40:00 +0100

    𝗧𝗢𝗚𝗘-𝗠𝗗-𝗩𝟮

--- src/plugin/repo.js
+++ src/plugin/repo.js
@@ -1,90 +1,6 @@
 import pkg, { prepareWAMessageMedia } from '@whiskeysockets/baileys';
 const { generateWAMessageFromContent, proto } = pkg;
-import axios from 'axios'; // Import axios for HTTP requests
-
-const handleRepoCommand = async (m, Matrix) => {
-  const repoUrl = 'https://api.github.com/repos/toge012345/TOGE-MD-V2';
-  try {
-    const response = await axios.get(repoUrl);
-    const repoData = response.data;
-
-    const { full_name, name, forks_count, stargazers_count, created_at, updated_at, owner } = repoData;
-
-    const messageText = `📊 Repository Information:
-    \n🔸 *Name:* ${name}
-    \n⭐ *Stars:* ${stargazers_count}
-    \n🍴 *Forks:* ${forks_count}
-    \n📅 *Created At:* ${new Date(created_at).toLocaleDateString()}
-    \n🛠️ *Last Updated:* ${new Date(updated_at).toLocaleDateString()}
-    \n👤 *Owner:* ${owner.login}`;
-
-    const repoMessage = generateWAMessageFromContent(m.from, {
-      viewOnceMessage: {
-        message: {
-          messageContextInfo: {
-            deviceListMetadata: {},
-            deviceListMetadataVersion: 2
-          },
-          interactiveMessage: proto.Message.InteractiveMessage.create({
-            body: proto.Message.InteractiveMessage.Body.create({
-              text: messageText
-            }),
-            footer: proto.Message.InteractiveMessage.Footer.create({
-              text: "𝐆𝐄𝐍𝐄𝐑𝐀𝐓𝐄𝐃 𝐁𝐘 𝐓𝐎𝐆𝐄-𝐌𝐃-𝐕𝟐"
-            }),
-            header: proto.Message.InteractiveMessage.Header.create({
-             ...(await prepareWAMessageMedia({ image: { url: `https://raw.githubusercontent.com/toge012345/TOGE-MD-V2/main/toge-v2.jpg` } }, { upload: Matrix.waUploadToServer })),
-              title: "",
-              gifPlayback: true,
-              subtitle: "",
-              hasMediaAttachment: false 
-            }),
-            nativeFlowMessage: proto.Message.InteractiveMessage.NativeFlowMessage.create({
-              buttons: [
-                {
-                  name: "quick_reply",
-                  buttonParamsJson: JSON.stringify({
-                    display_text: "Contact Owner",
-                    id: ".owner"
-                  })
-                },
-                {
-                  name: "cta_url",
-                  buttonParamsJson: JSON.stringify({
-                    display_text: "Click Here To Fork",
-                    url: `https://github.com/toge012345/TOGE-MD-V2/fork`
-                  })
-                },
-                {
-                  name: "cta_url",
-                  buttonParamsJson: JSON.stringify({
-                    display_text: "Join Our Community",
-                    url: `https://whatsapp.com/channel/0029VaiuD4s4IBhI0fzbv40Z`
-                  })
-                }
-              ],
-            }),
-            contextInfo: {
-              mentionedJid: [m.sender],
-              forwardingScore: 9999,
-              isForwarded: true,
-            }
-          }),
-        },
-      },
-    }, {});
-
-    await Matrix.relayMessage(repoMessage.key.remoteJid, repoMessage.message, {
-      messageId: repoMessage.key.id
-    });
-    await m.React("✅");
-
-  } catch (error) {
-    console.error("Error processing your request:", error);
-    m.reply('Error processing your request.');
-    await m.React("❌");
-  }
-};
+import axios from 'axios';
 
 const searchRepo = async (m, Matrix) => {
   const prefixMatch = m.body.match(/^[\\/!#.]/);
@@ -94,9 +10,105 @@ const searchRepo = async (m, Matrix) => {
   const validCommands = ['repo', 'sc', 'script'];
 
   if (validCommands.includes(cmd)) {
-    await handleRepoCommand(m, Matrix);
+    const repoUrl = `https://api.github.com/repos/toge012345/TOGE-MD-V2`;
+    
+    await handleRepoCommand(m, Matrix, repoUrl);
+  }
+};
+
+const handleRepoCommand = async (m, Matrix, repoUrl) => {
+  try {
+    const response = await axios.get(repoUrl);
+    const repoData = response.data;
+
+    const {
+      full_name,
+      name,
+      forks_count,
+      stargazers_count,
+      created_at,
+      updated_at,
+      owner,
+    } = repoData;
+
+    const messageText = `*_Repository Information:_*\n
+*_Name:_* ${name}
+*_Stars:_* ${stargazers_count}
+*_Forks:_* ${forks_count}
+*_Created At:_* ${new Date(created_at).toLocaleDateString()}
+*_Last Updated:_* ${new Date(updated_at).toLocaleDateString()}
+*_Owner:_* ${owner.login}
+    `;
+
+    const repoMessage = generateWAMessageFromContent(m.from, {
+      viewOnceMessage: {
+        message: {
+          messageContextInfo: {
+            deviceListMetadata: {},
+            deviceListMetadataVersion: 2,
+          },
+          interactiveMessage: proto.Message.InteractiveMessage.create({
+            body: proto.Message.InteractiveMessage.Body.create({
+              text: messageText,
+            }),
+            footer: proto.Message.InteractiveMessage.Footer.create({
+              text: '𝐆𝐄𝐍𝐄𝐑𝐀𝐓𝐄𝐃 𝐁𝐘 𝐓𝐎𝐆𝐄-𝐌𝐃-𝐕𝟐',
+            }),
+            header: proto.Message.InteractiveMessage.Header.create({
+              ...(await prepareWAMessageMedia({
+                image: {
+                  url: 'https://raw.githubusercontent.com/toge012345/TOGE-MD-V2/main/toge-v2.jpg',
+                },
+              }, { upload: Matrix.waUploadToServer })),
+              title: '',
+              gifPlayback: true,
+              subtitle: '',
+              hasMediaAttachment: false,
+            }),
+            nativeFlowMessage: proto.Message.InteractiveMessage.NativeFlowMessage.create({
+              buttons: [
+                {
+                  name: 'quick_reply',
+                  buttonParamsJson: JSON.stringify({
+                    display_text: 'Contact Owner',
+                    id: '.owner',
+                  }),
+                },
+                {
+                  name: 'cta_url',
+                  buttonParamsJson: JSON.stringify({
+                    display_text: 'Click Here To Fork',
+                    url: repoUrl.replace('api.', '').replace('repos/', '/forks/'),
+                  }),
+                },
+                {
+                  name: 'cta_url',
+                  buttonParamsJson: JSON.stringify({
+                    display_text: '𝚃𝚘𝙶𝚎 𝙸𝚗𝚄𝚖𝙰𝚔𝙸',
+                    url: 'https://whatsapp.com/channel/0029VaiuD4s4IBhI0fzbv40Z',
+                  }),
+                },
+              ],
+            }),
+            contextInfo: {
+              mentionedJid: [m.sender],
+              forwardingScore: 9999,
+              isForwarded: true,
+            },
+          }),
+        },
+      },
+    }, {});
+
+    await Matrix.relayMessage(repoMessage.key.remoteJid, repoMessage.message, {
+      messageId: repoMessage.key.id,
+    });
+    await m.React('✅');
+  } catch (error) {
+    console.error('Error processing your request:', error);
+    m.reply('Error processing your request.');
+    await m.React('❌');
   }
 };
 
 export default searchRepo;
-           
